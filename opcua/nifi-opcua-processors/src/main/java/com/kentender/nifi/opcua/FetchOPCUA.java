d9de5126c0abe94c6fcc580706da4a98418d1fcd
Wade Salazar <Wade@KenTender.com>
2016-11-17 21:46:55 -0600

    Working with security features (self signed cert)

--- opcua/nifi-opcua-processors/src/main/java/com/kentender/nifi/opcua/FetchOPCUA.java
+++ opcua/nifi-opcua-processors/src/main/java/com/kentender/nifi/opcua/FetchOPCUA.java
@@ -43,6 +43,7 @@ import org.apache.nifi.processor.util.StandardValidators;
 import org.opcfoundation.ua.application.Client;
 import org.opcfoundation.ua.application.SessionChannel;
 import org.opcfoundation.ua.builtintypes.DataValue;
+import org.opcfoundation.ua.builtintypes.LocalizedText;
 import org.opcfoundation.ua.builtintypes.NodeId;
 import org.opcfoundation.ua.common.ServiceFaultException;
 import org.opcfoundation.ua.common.ServiceResultException;
@@ -52,17 +53,33 @@ import org.opcfoundation.ua.core.ReadRequest;
 import org.opcfoundation.ua.core.ReadResponse;
 import org.opcfoundation.ua.core.ReadValueId;
 import org.opcfoundation.ua.core.TimestampsToReturn;
+import org.opcfoundation.ua.transport.security.Cert;
+import org.opcfoundation.ua.transport.security.KeyPair;
+import org.opcfoundation.ua.transport.security.PrivKey;
 import org.opcfoundation.ua.transport.security.SecurityPolicy;
+import org.opcfoundation.ua.utils.CertificateUtils;
 import  org.opcfoundation.ua.utils.EndpointUtil;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 @Tags({"OPC", "OPCUA", "UA"})
 @CapabilityDescription("Fetches a response from an OPC UA server based on configured name space and input item names")
@@ -74,11 +91,17 @@ import java.util.stream.Collectors;
 
 public class FetchOPCUA extends AbstractProcessor {
 	
+	public static final Locale ENGLISH = Locale.ENGLISH;
+	
 	// Create Client
-	Client myClient = Client.createClientApplication(null);
+	Client myClient = null;
 	EndpointDescription[] endpoints = null;
 	SessionChannel mySession = null;
 	ReadResponse res = null;
+	
+	//TODO obviously needs to be handled by a property
+	private static final String PRIVKEY_PASSWORD = "Opc.Ua";
+	
 
 	public static final PropertyDescriptor ENDPOINT = new PropertyDescriptor
             .Builder().name("Endpoint URL")
@@ -91,7 +114,7 @@ public class FetchOPCUA extends AbstractProcessor {
             .Builder().name("Security Policy")
             .description("How should Nifi authenticate with the UA server")
             .required(true)
-            .allowableValues("None", "Basic128Rsa15", "Basic256")
+            .allowableValues("None", "Basic128Rsa15")
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
     
@@ -177,25 +200,23 @@ public class FetchOPCUA extends AbstractProcessor {
     public void onScheduled(final ProcessContext context) {
     	
     	//TODO need to move the end point discovery task here
-
     }
 
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
-    	
+    	    		
     	final ComponentLog logger = getLogger();
-    	
+    	//Init response variable
+        final AtomicReference<String> reqTagname = new AtomicReference<>();
+        final AtomicReference<String> serverResponse = new AtomicReference<>();
+        
+        
+        //read tag name from flowfile input
         FlowFile flowFile = session.get();
         if ( flowFile == null ) {
             return;
         }
-        //Handle security issues
         
-        //Init response variable
-        final AtomicReference<String> reqTagname = new AtomicReference<>();
-        final AtomicReference<String> serverResponse = new AtomicReference<>();
-        
-        //read tag name from flowfile input
         session.read(flowFile, new InputStreamCallback() {
             @Override
             public void process(InputStream in) throws IOException {
@@ -216,33 +237,48 @@ public class FetchOPCUA extends AbstractProcessor {
         });
         
         
-        //discover endpoints
+        // Discover end points
         // TODO clarify if it is necessary to discover the endpoints or if we can let the user input them as a property
  		try {
- 			String url = context.getProperty(ENDPOINT).getValue();
- 			endpoints = myClient.discoverEndpoints(url);	
  			
- 			switch (context.getProperty(SECURITY_POLICY).getValue()) {
+ 			//select an endpoint
+			String url = context.getProperty(ENDPOINT).getValue();
 			
-				case("None"):{
-					logger.debug("The selected policy is " + context.getProperty(SECURITY_POLICY).getValue());				}
-					endpoints = selectBySecurityPolicy(endpoints,SecurityPolicy.NONE);
-				case("Basic128Rsa15"):{
-					logger.debug("The selected policy is " + context.getProperty(SECURITY_POLICY).getValue());
-					endpoints = selectBySecurityPolicy(endpoints,SecurityPolicy.NONE);
-				}
-				case("Basic256"):{
-					logger.debug("The selected policy is " + context.getProperty(SECURITY_POLICY).getValue());
-					endpoints = selectBySecurityPolicy(endpoints,SecurityPolicy.NONE);
-				}
-				// TODO other security policies can be added as needed 
-		
- 			}
+			switch (context.getProperty(SECURITY_POLICY).getValue()) {
+ 				case "None":{
+ 					myClient = Client.createClientApplication( null );
+ 					endpoints = myClient.discoverEndpoints(url);
+ 					endpoints = selectBySecurityPolicy(endpoints,SecurityPolicy.NONE);
+ 					
+ 					break;
+ 					
+ 				}
+ 				
+ 				case "Basic128Rsa15":{
+ 					
+ 					// Load Client's Application Instance Certificate from file
+ 					KeyPair myClientApplicationInstanceCertificate = getCert("Client");
+ 					KeyPair myHttpsCertificate = getHttpsCert("Client");
+ 					myClient = Client.createClientApplication( myClientApplicationInstanceCertificate );
+ 					myClient.getApplication().getHttpsSettings().setKeyPair(myHttpsCertificate);
+ 					endpoints = myClient.discoverEndpoints(url);
+ 					endpoints = selectBySecurityPolicy(endpoints,SecurityPolicy.BASIC128RSA15);
+ 					
+ 					break;
+ 					
+ 				}
+			
+			}
+			
+			endpoints = selectByProtocol(endpoints, "opc.tcp");
  			
- 			// TODO end point protocols can be added as needed
-     		endpoints = selectByProtocol(endpoints, "opc.tcp");
-     		
-     		
+ 			// Create Client
+			myClient.getApplication().addLocale( ENGLISH );
+			myClient.getApplication().setApplicationName( new LocalizedText("Java Sample Client", Locale.ENGLISH) );
+			myClient.getApplication().setProductUri( "urn:JavaSampleClient" );
+			
+			
+ 		
  		} catch (ServiceResultException e) {
  			// TODO Auto-generated catch block
  			e.printStackTrace();
@@ -286,7 +322,7 @@ public class FetchOPCUA extends AbstractProcessor {
   		try{
           	res = mySession.Read(req);
               DataValue[] values = res.getResults();
-              serverResponse.set(values[0].getValue().toString()  + ";"+ values[0].getServerTimestamp().toString() );
+              serverResponse.set(reqTagname.get() + ";" + values[0].getValue().toString()  + ";"+ values[0].getServerTimestamp().toString() );
               
           }catch (Exception e) {
   			// TODO Auto-generated catch block
@@ -317,4 +353,155 @@ public class FetchOPCUA extends AbstractProcessor {
 		}
         
     }
+    
+    public static KeyPair getCert(String applicationName) {
+		File certFile = new File(applicationName + ".der");
+		File privKeyFile =  new File(applicationName+ ".pem");
+		try {
+			Cert myServerCertificate = Cert.load( certFile );
+			PrivKey myServerPrivateKey = PrivKey.load( privKeyFile, PRIVKEY_PASSWORD );
+			return new KeyPair(myServerCertificate, myServerPrivateKey); 
+		} catch (CertificateException e) {
+			System.out.println(e.toString());
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println(e.toString());
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidParameterSpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {		
+			System.out.println("got an exception opening cert so creating a new cert?");
+			try {
+				String hostName = InetAddress.getLocalHost().getHostName();
+				String applicationUri = "urn:"+hostName+":"+applicationName;
+				KeyPair keys = CertificateUtils.createApplicationInstanceCertificate(applicationName, null, applicationUri, 3650, hostName);
+				keys.getCertificate().save(certFile);
+				keys.getPrivateKey().save(privKeyFile);
+				return keys;
+			} catch (Exception e1) {
+				System.out.println(e1.toString());
+			}
+		}
+		return null;
+	}
+	
+	public static KeyPair getHttpsCert(String applicationName){
+		File certFile = new File(applicationName + "_https.der");
+		File privKeyFile =  new File(applicationName+ "_https.pem");
+		try {
+			Cert myServerCertificate = Cert.load( certFile );
+			PrivKey myServerPrivateKey = PrivKey.load( privKeyFile, PRIVKEY_PASSWORD );
+			return new KeyPair(myServerCertificate, myServerPrivateKey); 
+		} catch (CertificateException e) {
+			
+			System.out.println(e.toString());
+		} catch (NoSuchAlgorithmException e) {
+			
+			System.out.println(e.toString());
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			
+			System.out.println(e.toString());
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (InvalidParameterSpecException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		} catch (IOException e) {	
+			System.out.println(e.toString());
+			System.out.println("got an exception so creating a new file?");
+			try {
+				KeyPair caCert = getCACert();
+				String hostName = InetAddress.getLocalHost().getHostName();
+				String applicationUri = "urn:"+hostName+":"+applicationName;
+				KeyPair keys = CertificateUtils.createHttpsCertificate(hostName, applicationUri, 3650, caCert);
+				keys.save(certFile, privKeyFile, PRIVKEY_PASSWORD);
+				return keys;
+			} catch (Exception e1) {
+				System.out.println(e1.toString());
+			}
+		}
+		return null;
+	}
+	
+	public static KeyPair getCACert(){
+		File certFile = new File("NifiCA.der");
+		File privKeyFile =  new File("NifiCA.pem");
+		try {
+			Cert myServerCertificate = Cert.load( certFile );
+			PrivKey myServerPrivateKey = PrivKey.load( privKeyFile, PRIVKEY_PASSWORD );
+			return new KeyPair(myServerCertificate, myServerPrivateKey); 
+		} catch (CertificateException e) {
+			System.out.println(e.toString());
+		} catch (IOException e) {		
+			try {
+				KeyPair keys = CertificateUtils.createIssuerCertificate("NifiCA", 3650, null);
+				keys.getCertificate().save(certFile);
+				keys.getPrivateKey().save(privKeyFile, PRIVKEY_PASSWORD);
+				return keys;
+			} catch (Exception e1) {
+				System.out.println(e1.toString());
+			}
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println(e.toString());
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidParameterSpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
